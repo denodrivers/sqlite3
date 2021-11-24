88a192315c5790d92b137f6df2a5f118c561e97e
DjDeveloperr <djsandhu2005@gmail.com>
2021-11-24 16:54:28 +0530

    fmt

--- test/test.ts
+++ test/test.ts
@@ -65,10 +65,9 @@ Deno.test("sqlite", async (t) => {
   });
 
   await t.step("query array", () => {
-    const row =
-      db.queryArray<[number, string, number, Uint8Array, null]>(
-        "select * from test where integer = 0",
-      )[0];
+    const row = db.queryArray<[number, string, number, Uint8Array, null]>(
+      "select * from test where integer = 0",
+    )[0];
     assertEquals(row[0], 0);
     assertEquals(row[1], "hello world");
     assertEquals(row[2], 3.14);
