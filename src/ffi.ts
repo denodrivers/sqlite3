1501825646d370d5bc4db7ac16439b3f2d58a500
DjDeveloperr <djsandhu2005@gmail.com>
2021-12-02 18:49:07 +0530

    ci

--- src/ffi.ts
+++ src/ffi.ts
@@ -6,301 +6,308 @@ import {
   SQLITE3_OPEN_READWRITE,
   SQLITE3_ROW,
 } from "./constants.ts";
-import {
-  cstr,
-  getPlatformFileName,
-  LITTLE_ENDIAN,
-  NULL_F64,
-  u64ToF64,
-} from "./util.ts";
+import { cstr, LITTLE_ENDIAN, NULL_F64, u64ToF64 } from "./util.ts";
 
-const lib = Deno.dlopen(
-  Deno.env.get("DENO_SQLITE_PATH") ??
-    new URL(`../${getPlatformFileName("sqlite3")}`, import.meta.url),
-  {
-    sqlite3_open_v2: {
-      parameters: [
-        "buffer", /* const char *path */
-        "buffer", /* sqlite3 **db */
-        "i32", /* int flags */
-        "u64", /* const char *zVfs */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_close_v2: {
-      parameters: ["f64" /* sqlite3 *db */],
-      result: "i32",
-    },
-
-    sqlite3_errmsg: {
-      parameters: ["f64" /* sqlite3 *db */],
-      result: "u64",
-    },
-
-    sqlite3_changes: {
-      parameters: ["f64" /* sqlite3 *db */],
-      result: "i32",
-    },
-
-    sqlite3_total_changes: {
-      parameters: ["f64" /* sqlite3 *db */],
-      result: "i32",
-    },
-
-    sqlite3_prepare_v3: {
-      parameters: [
-        "f64", /* sqlite3 *db */
-        "buffer", /* const char *sql */
-        "i32", /* int nByte */
-        "u32", /* prepFlags */
-        "buffer", /* sqlite3_stmt **ppStmt */
-        "buffer", /* const char **pzTail */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_libversion: {
-      parameters: [],
-      result: "u64",
-    },
-
-    sqlite3_step: {
-      parameters: ["f64" /* sqlite3_stmt *pStmt */],
-      result: "i32",
-    },
-
-    sqlite3_reset: {
-      parameters: ["f64" /* sqlite3_stmt *pStmt */],
-      result: "i32",
-    },
-
-    sqlite3_finalize: {
-      parameters: ["f64" /* sqlite3_stmt *pStmt */],
-      result: "i32",
-    },
-
-    sqlite3_bind_parameter_count: {
-      parameters: ["f64" /* sqlite3_stmt *pStmt */],
-      result: "i32",
-    },
-
-    sqlite3_bind_parameter_index: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "buffer", /* const char *zName */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_bind_parameter_name: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int i */
-      ],
-      result: "u64",
-    },
-
-    sqlite3_bind_blob: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int i */
-        "buffer", /* const void *zData */
-        "i32", /* int nData */
-        "f64", /* void (*xDel)(void*) */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_bind_blob64: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int i */
-        "buffer", /* const void *zData */
-        "u64", /* sqlite3_uint64 nData */
-        "f64", /* void (*xDel)(void*) */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_bind_double: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int i */
-        "f64", /* double rValue */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_bind_int: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int i */
-        "i32", /* int iValue */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_bind_int64: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int i */
-        "f64", /* sqlite3_int64 iValue */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_bind_null: {
-      parameters: ["f64", /* sqlite3_stmt *pStmt */ "i32" /* int i */],
-      result: "i32",
-    },
-
-    sqlite3_bind_text: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int i */
-        "buffer", /* const char *zData */
-        "i32", /* int nData */
-        "f64", /* void (*xDel)(void*) */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_bind_value: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int i */
-        "f64", /* sqlite3_value *pValue */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_bind_zeroblob: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int i */
-        "i32", /* int n */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_bind_zeroblob64: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int i */
-        "i64", /* sqlite3_uint64 n */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_exec: {
-      parameters: [
-        "f64", /* sqlite3 *db */
-        "buffer", /* const char *sql */
-        "f64", /* sqlite3_callback callback */
-        "f64", /* void *pArg */
-        "buffer", /* char **errmsg */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_column_blob: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int iCol */
-      ],
-      result: "u64",
-    },
-
-    sqlite3_column_double: {
-      parameters: ["f64", /* sqlite3_stmt *pStmt */ "i32" /* int iCol */],
-      result: "f64",
-    },
-
-    sqlite3_column_int: {
-      parameters: ["f64", /* sqlite3_stmt *pStmt */ "i32" /* int iCol */],
-      result: "i32",
-    },
-
-    sqlite3_column_int64: {
-      parameters: ["f64", /* sqlite3_stmt *pStmt */ "i32" /* int iCol */],
-      result: "i64",
-    },
-
-    sqlite3_column_text: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int iCol */
-      ],
-      result: "u64",
-    },
-
-    sqlite3_column_text16: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int iCol */
-      ],
-      result: "u64",
-    },
-
-    sqlite3_column_type: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int iCol */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_column_value: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int iCol */
-      ],
-      result: "u64",
-    },
-
-    sqlite3_column_bytes: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int iCol */
-      ],
-      result: "i32",
-    },
-
-    sqlite3_column_bytes16: {
-      parameters: ["f64", /* sqlite3_stmt *pStmt */ "i32" /* int iCol */],
-      result: "i32",
-    },
-
-    sqlite3_column_count: {
-      parameters: ["f64" /* sqlite3_stmt *pStmt */],
-      result: "i32",
-    },
-
-    sqlite3_column_name: {
-      parameters: [
-        "f64", /* sqlite3_stmt *pStmt */
-        "i32", /* int iCol */
-      ],
-      result: "u64",
-    },
-
-    sqlite3_free: {
-      parameters: ["f64"],
-      result: "void",
-    },
-
-    sqlite3_blob_bytes: {
-      parameters: ["f64"],
-      result: "i32",
-    },
-
-    sqlite3_blob_read: {
-      parameters: ["f64", "buffer", "i32", "i32"],
-      result: "i32",
-    },
+const symbols = <Record<string, Deno.ForeignFunction>> {
+  sqlite3_open_v2: {
+    parameters: [
+      "buffer", /* const char *path */
+      "buffer", /* sqlite3 **db */
+      "i32", /* int flags */
+      "u64", /* const char *zVfs */
+    ],
+    result: "i32",
   },
-);
+
+  sqlite3_close_v2: {
+    parameters: ["f64" /* sqlite3 *db */],
+    result: "i32",
+  },
+
+  sqlite3_errmsg: {
+    parameters: ["f64" /* sqlite3 *db */],
+    result: "u64",
+  },
+
+  sqlite3_changes: {
+    parameters: ["f64" /* sqlite3 *db */],
+    result: "i32",
+  },
+
+  sqlite3_total_changes: {
+    parameters: ["f64" /* sqlite3 *db */],
+    result: "i32",
+  },
+
+  sqlite3_prepare_v3: {
+    parameters: [
+      "f64", /* sqlite3 *db */
+      "buffer", /* const char *sql */
+      "i32", /* int nByte */
+      "u32", /* prepFlags */
+      "buffer", /* sqlite3_stmt **ppStmt */
+      "buffer", /* const char **pzTail */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_libversion: {
+    parameters: [],
+    result: "u64",
+  },
+
+  sqlite3_step: {
+    parameters: ["f64" /* sqlite3_stmt *pStmt */],
+    result: "i32",
+  },
+
+  sqlite3_reset: {
+    parameters: ["f64" /* sqlite3_stmt *pStmt */],
+    result: "i32",
+  },
+
+  sqlite3_finalize: {
+    parameters: ["f64" /* sqlite3_stmt *pStmt */],
+    result: "i32",
+  },
+
+  sqlite3_bind_parameter_count: {
+    parameters: ["f64" /* sqlite3_stmt *pStmt */],
+    result: "i32",
+  },
+
+  sqlite3_bind_parameter_index: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "buffer", /* const char *zName */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_bind_parameter_name: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int i */
+    ],
+    result: "u64",
+  },
+
+  sqlite3_bind_blob: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int i */
+      "buffer", /* const void *zData */
+      "i32", /* int nData */
+      "f64", /* void (*xDel)(void*) */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_bind_blob64: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int i */
+      "buffer", /* const void *zData */
+      "u64", /* sqlite3_uint64 nData */
+      "f64", /* void (*xDel)(void*) */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_bind_double: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int i */
+      "f64", /* double rValue */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_bind_int: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int i */
+      "i32", /* int iValue */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_bind_int64: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int i */
+      "f64", /* sqlite3_int64 iValue */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_bind_null: {
+    parameters: ["f64", /* sqlite3_stmt *pStmt */ "i32" /* int i */],
+    result: "i32",
+  },
+
+  sqlite3_bind_text: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int i */
+      "buffer", /* const char *zData */
+      "i32", /* int nData */
+      "f64", /* void (*xDel)(void*) */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_bind_value: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int i */
+      "f64", /* sqlite3_value *pValue */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_bind_zeroblob: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int i */
+      "i32", /* int n */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_bind_zeroblob64: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int i */
+      "i64", /* sqlite3_uint64 n */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_exec: {
+    parameters: [
+      "f64", /* sqlite3 *db */
+      "buffer", /* const char *sql */
+      "f64", /* sqlite3_callback callback */
+      "f64", /* void *pArg */
+      "buffer", /* char **errmsg */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_column_blob: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int iCol */
+    ],
+    result: "u64",
+  },
+
+  sqlite3_column_double: {
+    parameters: ["f64", /* sqlite3_stmt *pStmt */ "i32" /* int iCol */],
+    result: "f64",
+  },
+
+  sqlite3_column_int: {
+    parameters: ["f64", /* sqlite3_stmt *pStmt */ "i32" /* int iCol */],
+    result: "i32",
+  },
+
+  sqlite3_column_int64: {
+    parameters: ["f64", /* sqlite3_stmt *pStmt */ "i32" /* int iCol */],
+    result: "i64",
+  },
+
+  sqlite3_column_text: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int iCol */
+    ],
+    result: "u64",
+  },
+
+  sqlite3_column_text16: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int iCol */
+    ],
+    result: "u64",
+  },
+
+  sqlite3_column_type: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int iCol */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_column_value: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int iCol */
+    ],
+    result: "u64",
+  },
+
+  sqlite3_column_bytes: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int iCol */
+    ],
+    result: "i32",
+  },
+
+  sqlite3_column_bytes16: {
+    parameters: ["f64", /* sqlite3_stmt *pStmt */ "i32" /* int iCol */],
+    result: "i32",
+  },
+
+  sqlite3_column_count: {
+    parameters: ["f64" /* sqlite3_stmt *pStmt */],
+    result: "i32",
+  },
+
+  sqlite3_column_name: {
+    parameters: [
+      "f64", /* sqlite3_stmt *pStmt */
+      "i32", /* int iCol */
+    ],
+    result: "u64",
+  },
+
+  sqlite3_free: {
+    parameters: ["f64"],
+    result: "void",
+  },
+
+  sqlite3_blob_bytes: {
+    parameters: ["f64"],
+    result: "i32",
+  },
+
+  sqlite3_blob_read: {
+    parameters: ["f64", "buffer", "i32", "i32"],
+    result: "i32",
+  },
+};
+
+let lib: Deno.DynamicLibrary<typeof symbols>;
+
+const envSqlitePath = Deno.env.get("DENO_SQLITE_PATH");
+if (envSqlitePath !== undefined) {
+  lib = Deno.dlopen(envSqlitePath, symbols);
+} else {
+  try {
+    lib = Deno.dlopen("sqlite3", symbols);
+  } catch (e) {
+    const error = new Error(
+      "Built-in SQLite3 library not found, try installing SQLite3. If you have an existing installation, either add it to path or set the `DENO_SQLITE_PATH` environment variable.",
+    );
+    error.cause = e;
+    throw error;
+  }
+}
 
 export type sqlite3 = number;
 export type sqlite3_stmt = number;
